efe217f538c9318703cb1e140c19c80bebb92ced
busra <busracan39@gmail.com>
2018-11-19 18:51:56 +0200

    customer test case added. it is in the zip file. forgotten to push

--- src/main/java/com/entity/Customer.java
+++ src/main/java/com/entity/Customer.java
@@ -18,32 +18,16 @@ public class Customer implements Comparable<Customer> {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
     public double getLongitude() {
         return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
     }
 
     @Override
--- src/test/java/com/entity/CustomerTest.java
+++ src/test/java/com/entity/CustomerTest.java
@@ -1,5 +1,40 @@
+package com.entity;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CustomerTest {
 
+    private Customer customer = null;
+    @Before
+    public void setUp() throws Exception {
+        customer = new Customer(12, "Busra Canak", 34.000, -9.4444);
+    }
+
+    @Test
+    public void getId() {
+        assert customer.getId() == 12;
+    }
+
+    @Test
+    public void getName() {
+        assert customer.getName().equals("Busra Canak");
+    }
+
+    @Test
+    public void getLatitude() {
+        assert customer.getLatitude() == 34.000;
+    }
+
+    @Test
+    public void getLongitude() {
+        assert customer.getLongitude() == -9.4444;
+    }
+
+    @Test
+    public void toStringMethod() {
+        assert customer.toString().equals("[12: name = Busra Canak lat = 34.0 long = -9.4444]");
+    }
 }
